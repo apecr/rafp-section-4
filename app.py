350a79ae1746bc7b5a56487204a174070f93bf7d
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-03-23 09:36:48 +0100

    include login of endpoints
    Logic for errrors
    
    S4,L53

--- app.py
+++ app.py
@@ -1,15 +1,42 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
+items = []
 
-class Student(Resource):
+
+def get_item_by_name(name):
+    item = [item for item in items if item['name'] == name]
+    if item:
+        return item[0]
+    return None
+
+
+class Item(Resource):
     def get(self, name):
-        return {'student': name}
+        item = get_item_by_name(name)
+        if item:
+            return item
+        return {'item': None}, 404
+
+    def post(self, name):
+        item = get_item_by_name(name)
+        if not item:
+            data = request.get_json()
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+            return item, 201
+        return {'message': 'item {} already created'.format(name)}, 400
 
 
-api.add_resource(Student, '/student/<string:name>')
+class Items(Resource):
+    def get(self):
+        return items
 
-app.run(port=5002)
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(Items, '/items')
+
+app.run(port=5002, debug=True)
