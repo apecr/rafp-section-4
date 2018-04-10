a01ba51a3b1248fbf90e741a7814be7951e81300
alberto.eyo.contractor <alberto.eyo.contractor@bbva.com>
2018-04-10 08:47:31 +0200

    feat(app.py) Add parser
    Include parsing information in both POST and PUT /item/:name
    
    [FA-16]

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -12,12 +12,19 @@ items = []
 
 jwt = JWT(app, authenticate, identity)  # /auth
 
+parser = reqparse.RequestParser()
+parser.add_argument('price',
+                    type=float,
+                    required=True,
+                    help='This filed cannot be left blank')
+
 
 def get_item_by_name(name):
     return next(filter(lambda x: x['name'] == name, items), None)
 
 
 class Item(Resource):
+
     @jwt_required()
     def get(self, name):
         item = get_item_by_name(name)
@@ -27,7 +34,7 @@ class Item(Resource):
         item = get_item_by_name(name)
         if item:
             return {'message': 'item {} already created'.format(name)}, 400
-        data = request.get_json()
+        data = parser.parse_args()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
@@ -38,7 +45,8 @@ class Item(Resource):
         return {'message': 'Item deleted'}
 
     def put(self, name):
-        data = request.get_json()
+        data = parser.parse_args()
+
         item = next(filter(lambda x: x['name'] == name, items), None)
         if not item:
             item = {'name': name, 'price': data['price']}
